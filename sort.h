e067949a9d5b025f93c1de310771ce53a164b427
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-10 00:58:59 +0100

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -1,19 +1,20 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
-* struct listint_s - Doubly linked list node
-*
-* @n: Integer stored in the node
-* @prev: Pointer to the previous element of the list
-* @next: Pointer to the next element of the list
-*/
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
 typedef struct listint_s
 {
-	int n;
-
+	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
@@ -22,10 +23,12 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swap(int *xp, int *yp);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int *array, int i, int j, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
-void quick_sort_recursive(int *array, int low, int high, size_t size);
+void swap1(int arr[], int size, int *p1, int *p2);
+int partition(int arr[], int size, int low, int high);
+void sort(int arr[], int size, int low, int high);
+
 
 #endif
