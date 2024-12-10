e3d10326347f1d55c3c437e7976c73d41c1f6b3d
NaganoMSL228 <9794@holbertonstudents.com>
2024-12-10 01:09:31 +0100

    3-quick_sort.c

--- sort.h
+++ sort.h
@@ -1,7 +1,7 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H_
+#define _SORT_H_
 #include <stdio.h>
-#include <stddef.h>
+
 #include <stdlib.h>
 
 /**
@@ -19,16 +19,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void swap(int *xp, int *yp);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void swap1(int arr[], int size, int *p1, int *p2);
-int partition(int arr[], int size, int low, int high);
-void sort(int arr[], int size, int low, int high);
-
+void swap(int *array, size_t i, size_t j);
 
 #endif
